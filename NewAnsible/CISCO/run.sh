69bd398341445335bef999a252520187e85b06c4
Andrii Panin <andy.panin@gmail.com>
2021-08-04 12:42:01 +0300

    Add secret file

--- NewAnsible/CISCO/run.sh
+++ NewAnsible/CISCO/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ansible-playbook cisco.yaml -i hosts --ask-vault-pass
+#ansible-playbook cisco.yaml -i hosts --ask-vault-pass
+ansible-playbook cisco.yaml -i hosts --vault-password-file secret
+
